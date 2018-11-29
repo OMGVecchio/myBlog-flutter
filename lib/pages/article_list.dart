e0cfb99e3ec738a3355cedfc207261fe27400e17
vecchio <weichao@zbj.com>
2018-11-29 17:22:00 +0800

    简历+首页 tag 筛选

--- lib/pages/article_list.dart
+++ lib/pages/article_list.dart
@@ -9,12 +9,7 @@ Dio dio = Dio();
 class ArticleListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('文章列表'),
-      ),
-      body: ArticleList(),
-    );
+    return ArticleList();
   }
 }
 
@@ -25,52 +20,89 @@ class ArticleList extends StatefulWidget {
 
 class ArticleListState extends State {
   final articleList = [];
+  final articleTags = [];
+  var realArticleList = [];
   ArticleListState() {
-    this._fetchArticleList();
+    this._fetchArticleData();
   }
-  void _fetchArticleList() async {
-    Response res = await dio.get('${ArticleUtils.host}/api/article');
+  void _fetchArticleData() async {
+    Response listRes = await dio.get('${ArticleUtils.host}/api/article');
+    Response tagsRes = await dio.get('${ArticleUtils.host}/api/tags');
     setState(() {
-      articleList.addAll(res.data['data']);  
+      articleList.addAll(listRes.data['data']);
+      articleTags.addAll(tagsRes.data['data']);
+      realArticleList = articleList;
     });
   }
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      padding: EdgeInsets.all(16.0),
-      itemCount: articleList.length,
-      itemBuilder: (ctx, i) {
-        if (i < articleList.length) {
-          final articleInfo = articleList[i];
-          final coverUrl = articleInfo['cover'];
-          final articleCover = ArticleUtils.getCoverUrl(coverUrl);
-          return Card(
-            child: Column(
-              children: <Widget>[
-                Center(
-                  child: Image.network(
-                    articleCover,
-                    fit: BoxFit.fitHeight,
-                    height: 200,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('文章列表'),
+        actions: <Widget>[
+          PopupMenuButton(
+            onSelected: (tag) {
+              setState(() {
+                var newArticleList = [];
+                articleList.forEach((article) {
+                  var tags = article['tags'];
+                  if (tags == null || (tags != null && tags.length != 0 && tags.indexOf(tag) != -1)) {
+                    newArticleList.add(article);
+                  }
+                });
+                setState(() {
+                  realArticleList = newArticleList;
+                });
+              });
+            },
+            itemBuilder: (context) {
+              return articleTags.map((tag) {
+                return PopupMenuItem(
+                  // value 是 onSelected 里选中的值
+                  value: tag,
+                  child: Text(tag),
+                );
+              }).toList();
+            },
+          ),
+        ],
+      ),
+      body: ListView.builder(
+        padding: EdgeInsets.all(16.0),
+        itemCount: realArticleList.length,
+        itemBuilder: (ctx, i) {
+          if (i < realArticleList.length) {
+            final articleInfo = realArticleList[i];
+            final coverUrl = articleInfo['cover'];
+            final articleCover = ArticleUtils.getCoverUrl(coverUrl);
+            return Card(
+              child: Column(
+                children: <Widget>[
+                  Center(
+                    child: Image.network(
+                      articleCover,
+                      fit: BoxFit.fitHeight,
+                      height: 200,
+                    ),
                   ),
-                ),
-                ListTile(
-                  title: Text(articleInfo['title']),
-                  subtitle: Text(articleInfo['desc']),
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (ctx) => ArticleDetailPage(articleInfo['id']),
-                      ),
-                    );
-                  },
-                ),
-              ],
-            ),
-          );
-        }
-      },
+                  ListTile(
+                    title: Text(articleInfo['title']),
+                    subtitle: Text(articleInfo['desc']),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (ctx) => ArticleDetailPage(articleInfo['id']),
+                        ),
+                      );
+                    },
+                  ),
+                ],
+              ),
+            );
+          }
+        },
+      ),
     );
   }
 }
--- lib/pages/curriculum_vitae.dart
+++ lib/pages/curriculum_vitae.dart
@@ -5,9 +5,74 @@ class CurriculumVitaePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('简历'),
+        title: Text('生涯'),
       ),
-      body: Text('简历'),
+      body: ListView(
+        children: <Widget>[
+          Container(
+            child: Column(
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: Stack(
+                        alignment: Alignment(0, 3),
+                        children: <Widget>[
+                          Container(
+                            height: 150,
+                            decoration: BoxDecoration(
+                              image: DecorationImage(
+                                image: AssetImage('assets/images/cats.gif'),
+                                fit: BoxFit.fitWidth,
+                              )
+                            ),
+                          ),
+                          Container(
+                            height: 100,
+                            width: 100,
+                            child: CircleAvatar(
+                              backgroundImage: AssetImage('assets/images/octocat.png'),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          Row(
+            children: <Widget>[
+              Expanded(
+                child: Container(
+                  margin: EdgeInsets.only(top: 40),
+                  padding: EdgeInsets.all(20),
+                  child: Column(
+                    children: <Widget>[
+                      ListTile(
+                        title: Text('Vue/Nuxt/Weex'),
+                      ),
+                      ListTile(
+                        title: Text('React/Next/ReactNative'),
+                      ),
+                      ListTile(
+                        title: Text('Flutter'),
+                      ),
+                      ListTile(
+                        title: Text('Nodejs'),
+                      ),
+                      ListTile(
+                        title: Text('Webpack'),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
